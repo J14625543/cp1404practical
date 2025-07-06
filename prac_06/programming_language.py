7aeee12dfe706675514145701e032062c520601a
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-06 21:19:00 +0800

    Initializes a new ProgrammingLanguage object with:
    name: name of the language
    
    typing: typing system ("Dynamic" or "Static")
    
    reflection: whether it supports reflection (True/False)
    
    year: year of first appearance

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,1 +1,8 @@
 class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
