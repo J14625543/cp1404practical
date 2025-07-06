f6049e8d1d75b403925bb8f4d1e336d72ebea50e
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-06 21:10:05 +0800

    Adds two hardcoded guitar objects to the list for testing or demonstration.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -12,4 +12,8 @@ def main():
         print(guitar_to_add, "added.")
         name = input("Name: ")
 
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+
 
