dc3ed0ea00be1f27b6293ee54df6c9e2c46ba79d
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-06 20:47:09 +0800

    Simulates driving:
    If not enough fuel, drive as far as possible.
    
    Updates fuel and odometer.
    
    Returns distance actually driven.

--- prac_06/car.py
+++ prac_06/car.py
@@ -6,3 +6,12 @@ class Car:
     def add_fuel(self, amount):
             self.fuel += amount
 
+    def drive(self, distance):
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self._odometer += distance
+        return distance
+
