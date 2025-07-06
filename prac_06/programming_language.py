74e4a33df2ca3a652582e0428b6c054541fa1ddf
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-06 21:21:22 +0800

    Creates test instances of programming languages, prints one of them, and then prints the names of those with dynamic typing.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -12,6 +12,18 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         return self.typing == "Dynamic"
 
+def run_tests():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    languages = [ruby, python, visual_basic]
+    print(python)
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
 
 
 
