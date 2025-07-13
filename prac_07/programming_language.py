2a510025b404c81470e9ad72b862ae6eca339d68
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-13 19:53:49 +0800

    Creates some sample ProgrammingLanguage instances and demonstrates usage of the class.

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -16,6 +16,19 @@ class ProgrammingLanguage:
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    languages = [ruby, python, visual_basic]
+    print(python)
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
 
 
 
