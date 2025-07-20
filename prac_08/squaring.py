e60c2edf17b3a4cdee02cd0b2aec80c639a31402
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-20 20:42:45 +0800

    Define the main class for the app by inheriting from App.
    This class controls how the app initializes, processes user input, and updates the interface.

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -2,3 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+
+class NumberSquaringApp(App):
+    """A simple Kivy app to compute the square of a number."""
+
