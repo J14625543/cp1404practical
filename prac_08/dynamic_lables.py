055f6961922a41f55e41256d411ee59fa8a10e3d
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-20 21:00:38 +0800

    When the app starts, dynamically create a Label for each name in the list and add it to the layout identified by main.

--- prac_08/dynamic_lables.py
+++ prac_08/dynamic_lables.py
@@ -11,3 +11,11 @@ class NameListApp(App):
     def build(self):
         return Builder.load_file('dynamic_labels.kv')
 
+    def on_start(self):
+        container = self.root.ids.main
+        for user in self.user_names:
+            new_label = Label(text=user)
+            container.add_widget(new_label)
+
+
+
