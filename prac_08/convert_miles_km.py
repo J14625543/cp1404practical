531a273d8b8df3cb327ed96d73d96f09746cbc16
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-20 20:36:32 +0800

    Set the app window title and load the interface layout from a .kv file.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,4 +7,9 @@ MILES_TO_KILOMETERS = 1.60934
 class DistanceConverterApp(App):
     """A Kivy App for converting miles into kilometers."""
 
+    def build(self):
+        """Initialize the application window and load the interface."""
+        self.title = "Miles to Kilometers Converter"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
 
