e2de0314c0942996a3481b1b9eda25c14e4d81d6
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 19:53:48 +0800

    Defines a class named Vehicle.
    The docstrings explain the purpose of the file and class.

---
+++ prac_09/car.py
@@ -0,0 +1,9 @@
+"""
+Vehicle class for CP1404 Practical 9
+Simulates a simple car with fuel and distance tracking.
+"""
+
+
+class Vehicle:
+    """A basic vehicle model with fuel usage and distance record."""
+
