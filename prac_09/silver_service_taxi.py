6ac468e180cddd3209cb7243f1dbdd3493035463
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 20:03:34 +0800

    Customizes how the LuxuryTaxi appears when printed. Redefines how the total fare is calculated.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -17,4 +17,12 @@ class LuxuryTaxi(Taxi):
         self.luxury_multiplier = luxury_level
         self.price_per_km *= self.luxury_multiplier
 
+    def __str__(self):
+        """Return formatted string showing taxi details and flagfall."""
+        parent_str = super().__str__()
+        return f"{parent_str} | includes flagfall ${self.BASE_FLAGFALL:.2f}"
 
+    def calculate_fare(self):
+        """Return total fare including flagfall."""
+        base_fare = super().get_fare()
+        return base_fare + self.BASE_FLAGFALL
