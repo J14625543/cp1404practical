39ceb53d7e2b7d32f16591799d8de6c3f1950351
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 20:26:35 +0800

    Import the classes Car, Taxi, and SilverServiceTaxi. Define the menu string for user options.

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,11 @@
+"""
+Taxi Simulator for CP1404 Practical 9
+"""
+
+from car import Car
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU_OPTIONS = "q)uit, c)hoose taxi, d)rive"
+
+
