25abc8f52fb10826efa4fe37827d061aa0cc393d
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 20:40:50 +0800

    Define a function called test_unreliable_cars() that will demonstrate how cars with different reliability percentages behave when trying to drive.
    Create two UnreliableCar objects:
    
    reliable_car has 90% reliability (usually successful).
    
    unreliable_car has 9% reliability (rarely successful).

--- prac_09/unreliavle_car_test.py
+++ prac_09/unreliavle_car_test.py
@@ -4,4 +4,8 @@ Test program for UnreliableCar class
 
 from unreliable_car import UnreliableCar
 
+def test_unreliable_cars():
+    """Demonstrate how UnreliableCar behaves with different reliability levels."""
+    reliable_car = UnreliableCar("Reliable One", 100, 90)
+    unreliable_car = UnreliableCar("Sketchy Ride", 100, 9)
 
