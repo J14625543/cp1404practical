830f0b00f58d6016767755e3f24b6aa8a6fc8f63
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 20:02:43 +0800

    Defines a fixed charge (flagfall) that will be added to every fare.   Initializes a new LuxuryTaxi object.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,4 +9,12 @@ from taxi import Taxi
 class LuxuryTaxi(Taxi):
     """A premium taxi that includes an extra flagfall fee."""
 
+    BASE_FLAGFALL = 4.5
+
+    def __init__(self, label, fuel_amount, luxury_level):
+        """Create a LuxuryTaxi instance with given name, fuel, and luxury multiplier."""
+        super().__init__(label, fuel_amount)
+        self.luxury_multiplier = luxury_level
+        self.price_per_km *= self.luxury_multiplier
+
 
