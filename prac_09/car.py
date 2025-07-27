443780f5da1e8c7eff4eebacbc75938f2050bd68
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 19:54:41 +0800

    Sets up the vehicle with a name and starting fuel.
    Initializes the odometer (distance) to 0.

--- prac_09/car.py
+++ prac_09/car.py
@@ -7,3 +7,16 @@ Simulates a simple car with fuel and distance tracking.
 class Vehicle:
     """A basic vehicle model with fuel usage and distance record."""
 
+    def __init__(self, model_name="", initial_fuel=0):
+        """
+        Create a new Vehicle instance.
+
+        Args:
+            model_name (str): name of the vehicle
+            initial_fuel (float): starting amount of fuel
+        """
+        self.model_name = model_name
+        self.fuel_level = initial_fuel
+        self.distance_travelled = 0
+
+
