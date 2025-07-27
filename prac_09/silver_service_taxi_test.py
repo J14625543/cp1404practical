3bc2855e54cce910872adde3659299411c9d9981
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 19:46:26 +0800

    print(fancy_taxi) calls the class’s __str__() method to display the taxi’s current state.
    get_fare() calculates the cost of the trip based on distance and fanciness.
    
    The fare is printed with 2 decimal places for formatting.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -10,4 +10,10 @@ def demo_silver_taxi():
     fancy_taxi = SilverServiceTaxi(name="Demo Silver Taxi", fuel=100, fanciness=2)
     fancy_taxi.drive(distance=18)
 
+    print(fancy_taxi)
+    fare = fancy_taxi.get_fare()
+    print(f"Fare for the trip: ${fare:.2f}")
 
+
+if __name__ == "__main__":
+    demo_silver_taxi()
