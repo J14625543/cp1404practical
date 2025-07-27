0116cdb390b32fee00b2a36578d504719f611383
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 20:23:19 +0800

    Make a Taxi object named "Prius 1" with 100 units of fuel, drive it for 40 km, print its status, start a new fare, drive 100 km more, then print status again.

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -6,4 +6,9 @@ from taxi import Taxi
 
 def run_taxi_demo():
     """Demonstrate Taxi class usage."""
-
+    taxi_instance = Taxi("Prius 1", 100)
+    taxi_instance.drive(40)
+    print(taxi_instance)
+    taxi_instance.start_fare()
+    taxi_instance.drive(100)
+    print(taxi_instance)
