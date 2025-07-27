f823b8051ce44837a092f81e92d87d6f8a83f08d
J14625543 <160458797+J14625543@users.noreply.github.com>
2025-07-27 20:27:23 +0800

    Initialize the total bill to zero. Create a list of taxis including regular and luxury taxis. Set the currently selected taxi to None. Print welcome message and menu.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,4 +8,16 @@ from silver_service_taxi import SilverServiceTaxi
 
 MENU_OPTIONS = "q)uit, c)hoose taxi, d)rive"
 
+def taxi_simulator():
+    """Run the taxi simulator."""
+    total_cost = 0
+    fleet = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    selected_taxi = None
+
+    print("Welcome to the Taxi Simulator!")
+    print(MENU_OPTIONS)
 
